d6c11219c40e9015cf40b63b8c79d68e24846674
Anna <anuta2519@yandex.ru>
2024-07-23 23:19:02 +0300

    add diploma

--- TourApi.py
+++ TourApi.py
@@ -4,21 +4,90 @@ class TourApi:
     # Инициализация 
     def __init__(self, url) -> None:
         self.url = url
+    
+    def get_city_id(self,city_name):
+        response = requests.get(self.url+ '/filters/DepartureCities')
+        # Проверка статуса ответа
+        if response.status_code == 200:
+            try:
+                cities = response.json()
+                for city in cities:
+                    if city['name'].lower() == city_name.lower():
+                        return city['id']
+                return f"City '{city_name}' not found in the list."
+            except ValueError:
+                return "Error decoding JSON from the response."
+        else:
+            return f"Error: Unable to fetch data, status code {response.status_code}"
 
-    def search(self,city, country , start_date, nights_count, adults):
+    def get_coutry_id(self,city_name):
+        response = requests.get(self.url+'/geoip')
+        # Проверка статуса ответа
+        if response.status_code == 200:
+            try:
+                cities = response.json()
+                for city in cities:
+                    if city['name'].lower() == city_name.lower():
+                        return city['id']
+                return f"City '{city_name}' not found in the list."
+            except ValueError:
+                return "Error decoding JSON from the response."
+        else:
+            return f"Error: Unable to fetch data, status code {response.status_code}"
+
+
+    def search(self,id_city, id_country , start_date, nights_count, adults):
         params = {
-            "city": city,
-            "country": country,
-            "start_date": start_date,
-            "nights_count": nights_count,
+            "departureCityId": id_city,
+            "country": id_country,
+            "minStartDate": start_date,
+            "minNightsCount": nights_count,
             "adults": adults
         }
-        resp = requests.get(self.url , params=params)
-        if resp.status_code != 200:
-            raise Exception(f"Error: {resp.status_code} - {resp.text}")
-        # Проверка содержания ответа
-        try:
-            return resp.json()
-        except requests.exceptions.JSONDecodeError:
-            raise Exception(f"Response is not JSON: {resp.text}")
+        response = requests.get(self.url , params=params)
+        if response.status_code == 200:
+            try:
+                return response.json()
+            except requests.exceptions.JSONDecodeError:
+                print("Error decoding JSON from the response.")
+                print("Response content:", response.text)
+                return None
+        else:
+            print(f"Error: Unable to fetch data, status code {response.status_code}")
+            return None
 
+    def get_hotel_ids(self):
+        response = requests.get(self.url+"/geo")
+        if response.status_code == 200:
+            try:
+                data = response.json()
+                hotels = data.get('hotels', [])
+                hotel_ids = [hotel.get('id') for hotel in hotels if hotel.get('id') is not None]
+                return hotel_ids
+            except requests.exceptions.JSONDecodeError:
+                print("Error decoding JSON from the response.")
+                print("Response content:", response.text)
+                return []
+        else:
+            print(f"Error: Unable to fetch data, status code {response.status_code}")
+            return []
+  
+    def like(self, id_hotel):
+        params={
+            "objectKey":id_hotel
+            }
+        response = requests.get(self.url+'/search/wishList',params=params )
+        assert response.status_code == 200
+        return response.json()
+    
+    def list_like(self, params_to_add=None):
+        resp = requests.get(self.url + '/wishlist', params=params_to_add)
+        return resp
+
+    
+    def delete_like(self, id_hotel):
+        params={
+            "objectKey":id_hotel
+            }
+        response = requests.delete(self.url+'/search/wishList/' + str(id_hotel) ,params=params )
+    
--- test_api.py
+++ test_api.py
@@ -2,16 +2,80 @@ import requests
 from TourApi import TourApi
 
 api = TourApi("https://fstravel.com/searchtour")
+api2=TourApi("https://fstravel.com/api")
+url = TourApi("https://fstravel.com")
 
-#Поиск тура
+#Поиск id города отправления
+def test_id_city():
+    city_name = "Санкт-Петербург"
+    city_id = api2.get_city_id(city_name)
+    print(f"The ID for {city_name} is {city_id}")
+
+#Поиск id страны пребывания
+def test_id_country():
+    country_name = "Турция"
+    country_id = api2.get_coutry_id(country_name)
+    print(f"The ID for {country_name} is {country_id}")
+
+# #Поиск тура
 def test_search_tour():
-    city = "Санкт-Петербург"
-    country = "Турция"
+    city_name = "Санкт-Петербург"
+    city_id = api2.get_city_id(city_name) 
+    country_name = "Турция"
+    country_id = api2.get_coutry_id(country_name)
+    city = city_id
+    country = country_id
     start_date = "2024-08-01"
     nights_count = 7
     adults = 2
-    body = api.search(city, country, start_date, nights_count, adults)
-    assert body['status'] == 200
-    assert "tours" in body
-    assert isinstance(body["tours"], list)
-    assert len(body["tours"]) > 0
+
+    result = api.search(city_id, country_id, start_date, nights_count, adults)
+    if result:
+        print(result)
+    else:
+        print("Failed to retrieve tour information.")
+
+def test_hotel_id():
+    hotel_ids =api2.get_hotel_ids
+    print("Hotel IDs:", hotel_ids)
+
+# #Добавление тура в избранное
+def test_like_tour():
+    #нашли тур
+    city_name = "Санкт-Петербург"
+    city_id = api2.get_city_id(city_name) 
+    country_name = "Турция"
+    country_id = api2.get_coutry_id(country_name)
+    city = city_id
+    country = country_id
+    start_date = "2024-08-01"
+    nights_count = 7
+    adults = 2
+    result = api.search(city_id, country_id, start_date, nights_count, adults)
+    body1_like =url.list_like()
+    #Поиск id отеля
+    id_hotel =api2.get_hotel_ids
+    hotel_like =api2.like(id_hotel)
+    body2_like = url.list_like()
+    assert len(body1_like) + 1 == len(body2_like)
+
+def test_delete_like_tour():
+    #нашли тур
+    city_name = "Санкт-Петербург"
+    city_id = api2.get_city_id(city_name) 
+    country_name = "Турция"
+    country_id = api2.get_coutry_id(country_name)
+    city = city_id
+    country = country_id
+    start_date = "2024-08-01"
+    nights_count = 7
+    adults = 2
+    result = api.search(city_id, country_id, start_date, nights_count, adults)
+    
+    #Поиск id отеля
+    id_hotel =api2.get_hotel_ids
+    #добавление в избранное 
+    body1_like =api2.like(id_hotel)
+    #удаление из избранного
+    body2_delete = api2.delete_like(body1_like)
+   
